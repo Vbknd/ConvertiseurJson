71252f8fecd2895bdf348209a8425b93ad741df6
SoulSndr <oskanianvictor@yahoo.com>
2025-04-11 18:29:54 +0200

    fileChooser qualité de vie

--- src/convertisseur/ConvertisseurApp.java
+++ src/convertisseur/ConvertisseurApp.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class ConvertisseurApp {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Convertisseur JSON (CSV/SQL) - SOLID");
+            JFrame frame = new JFrame("Convertisseur JSON (CSV/SQL)");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(600, 450);
             frame.setLocationRelativeTo(null);
